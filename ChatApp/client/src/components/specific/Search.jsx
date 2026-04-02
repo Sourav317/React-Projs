5b1cf28d19e242d83265ae43423fce114f9321a7
Sourav317 <kediasourav2@gmail.com>
2026-04-02 08:43:02 +0530

    Added find friend

--- ChatApp/client/src/components/specific/Search.jsx
+++ ChatApp/client/src/components/specific/Search.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useAsyncMutation } from "../../hooks/hook";
+// import {
+//   useLazySearchUserQuery,
+//   useSendFriendRequestMutation,
+// } from "../../redux/api/api";
+// import { setIsSearch } from "../../redux/reducers/misc";
+import UserItem from "../shared/UserItem";
+import { sampleUsers } from "../../constants/SampleData";
 
-function Search() {
+const Search = () => {
+  // const { isSearch } = useSelector((state) => state.misc);
+
+  // const [searchUser] = useLazySearchUserQuery();
+  let isLoadingSendFriendRequest = false;
+  // const [sendFriendRequest, isLoadingSendFriendRequest] = useAsyncMutation(
+  //   useSendFriendRequestMutation
+  // );
+
+  // const dispatch = useDispatch();
+
+  const search = useInputValidation("");
+
+  const [users, setUsers] = useState(sampleUsers);
+
+  const addFriendHandler = async (id) => {
+    console.log("add friend", id);
+    // await sendFriendRequest("Sending friend request...", { userId: id });
+  };
+
+  // const searchCloseHandler = () => dispatch(setIsSearch(false));
+
+  // useEffect(() => {
+  //   const timeOutId = setTimeout(() => {
+  //     searchUser(search.value)
+  //       .then(({ data }) => setUsers(data.users))
+  //       .catch((e) => console.log(e));
+  //   }, 1000);
+
+  //   return () => {
+  //     clearTimeout(timeOutId);
+  //   };
+  // }, [search.value]);
+
   return (
-    <div>Search</div>
-  )
-}
+    <Dialog open={true}>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
 
-export default Search
+        <List>
+          {users.map((i) => (
+            <UserItem
+              user={i}
+              key={i._id}
+              handler={addFriendHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+            />
+          ))}
+        </List>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default Search;
